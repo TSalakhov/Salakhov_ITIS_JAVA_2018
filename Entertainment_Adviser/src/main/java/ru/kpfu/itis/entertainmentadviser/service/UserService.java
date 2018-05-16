44a1b60b22cf4b945bb0d46c5df212898e79534c
TSalakhov <tim.salah.1601@gmail.com>
2018-05-16 15:28:16 +0300

    g

--- Entertainment_Adviser/src/main/java/ru/kpfu/itis/entertainmentadviser/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package ru.kpfu.itis.entertainmentadviser.service;
-
-import ru.kpfu.itis.entertainmentadviser.model.User;
-
-public interface UserService {
-    User getById(Long id);
-    User getByUsername(String username);
-}
